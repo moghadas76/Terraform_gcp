d576d9bb26b4cae2084a335ac6e52b52bcf96b3e
moghadas76 <s.m.moghadas2012@gmail.com>
2024-11-26 14:53:53 +0100

    Version

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+  
+  # Configure backend for state storage
+  backend "gcs" {
+    bucket = "my-terraform-state-bucket"
+    prefix = "terraform/state"
+  }
+}
